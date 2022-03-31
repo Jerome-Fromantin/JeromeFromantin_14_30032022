db7df7533a6922a45e1018ecc59c07c733b721c1
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-03-31 18:40:27 +0200

    Implémentation partielle de la 2ème page

--- src/pages/employeeList.js
+++ src/pages/employeeList.js
@@ -1,4 +1,13 @@
+import { Link } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+`
 
 const EmployeeList = () => {
     return (
@@ -6,7 +15,11 @@ const EmployeeList = () => {
             <Helmet>
                 <title>HRnet - Current Employees</title>
             </Helmet>
-            <h1>Current Employees</h1>
+            <Container id="employee-div">
+                <h1>Current Employees</h1>
+                <table id="employee-table" class="display"></table>
+                <Link to="/">Home</Link>
+            </Container>
         </HelmetProvider>)
 }
 
