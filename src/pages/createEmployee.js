f936c06303493c8255f951de18be6488366bb698
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-04-01 18:21:30 +0200

    Mise en place du date picker dans 2 champs de formulaire

--- src/pages/createEmployee.js
+++ src/pages/createEmployee.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import styled from 'styled-components'
+import Datetime from 'react-datetime'
+import 'react-datetime/css/react-datetime.css'
+import '../index.css'
 
 // Use of styled components.
 const Title = styled.div`
@@ -53,6 +56,7 @@ const CreateEmployee = () => {
     const [startDateInputValue, setStartDateInputValue] = useState("")
     const [streetInputValue, setStreetInputValue] = useState("")
     const [cityInputValue, setCityInputValue] = useState("")
+    const [zipCodeInputValue, setZipCodeInputValue] = useState("")
     // The different "useState" above allow to keep the data of the different fields in the form.
 
     // This function (to improve) displays an error message when a value in a field is invalid
@@ -65,10 +69,10 @@ const CreateEmployee = () => {
             alert("The last name field is invalid.")
         }
         else if (birthDateInputValue === "") {
-            alert("The birth date field must use a React date picker still not set up.")
+            alert("The birth date field is empty.")
         }
         else if (startDateInputValue === "") {
-            alert("The start date field must use a React date picker still not set up.")
+            alert("The start date field is empty.")
         }
         else if (streetInputValue === "" || streetInputValue.length < 2) {
             alert("The street field is invalid.")
@@ -76,7 +80,15 @@ const CreateEmployee = () => {
         else if (cityInputValue === "" || !/^[^\d]+$/.test(cityInputValue)) {
             alert("The city field is invalid.")
         }
+        else if (zipCodeInputValue === "" || zipCodeInputValue < 10000 || zipCodeInputValue >= 100000) {
+            alert("The zip code field is invalid.")
+        }
         else {alert("Employee Created!")}
+    }
+
+    // Customization of the input in the <Datetime/> component.
+    let inputProps = {
+        className: "dateLib"
     }
     
     return (
@@ -95,30 +107,35 @@ const CreateEmployee = () => {
                         <Label htmlFor="first-name">First Name</Label>
                         <Input type="text" id="first-name" value={firstNameInputValue}
                         onChange={(e) => setFirstNameInputValue(e.target.value)}/><br/>
-                    <Button onClick={saveEmployee2}>Save</Button>
+                        
+                        <Button onClick={saveEmployee2}>Save</Button>
+                        <span>&nbsp;Bouton provisoire pour test</span>
 
                         <Label htmlFor="last-name">Last Name</Label>
                         <Input type="text" id="last-name" value={lastNameInputValue}
                         onChange={(e) => setLastNameInputValue(e.target.value)}/>
 
                         <Label htmlFor="date-of-birth">Date of Birth</Label>
-                        <Input type="text" id="date-of-birth" value={birthDateInputValue}
-                        onChange={(e) => setBirthDateInputValue(e.target.value)}/>
+                        <Datetime id="date-of-birth" inputProps={inputProps} value={birthDateInputValue}
+                        onChange={(value) => setBirthDateInputValue(value)} closeOnSelect={true} timeFormat={false}/>
 
                         <Label htmlFor="start-date">Start Date</Label>
-                        <Input type="text" id="start-date" value={startDateInputValue}
-                        onChange={(e) => setStartDateInputValue(e.target.value)}/>
+                        <Datetime id="start-date" inputProps={inputProps} value={startDateInputValue}
+                        onChange={(value) => setStartDateInputValue(value)} closeOnSelect={true} timeFormat={false}/>
+                        {/* "inputProps" allows to customize the appearance of the input,
+                        "closeOnSelect" closes the picker when a day is clicked upon and
+                        "timeFormat" hides the time of the day in the picker and in the input.*/}
 
                         <Fieldset className="address">
                             <legend>Address</legend>
 
                             <Label htmlFor="street">Street</Label>
                             <Input type="text" id="street" value={streetInputValue}
-                        onChange={(e) => setStreetInputValue(e.target.value)}/>
+                            onChange={(e) => setStreetInputValue(e.target.value)}/>
 
                             <Label htmlFor="city">City</Label>
                             <Input type="text" id="city" value={cityInputValue}
-                        onChange={(e) => setCityInputValue(e.target.value)}/>
+                            onChange={(e) => setCityInputValue(e.target.value)}/>
 
                             <Label htmlFor="state">State</Label>
                             <Select name="state" id="state">
@@ -184,7 +201,8 @@ const CreateEmployee = () => {
                             </Select>
 
                             <Label htmlFor="zip-code">Zip Code</Label>
-                            <Input id="zip-code" type="number" />
+                            <Input type="number" id="zip-code" value={zipCodeInputValue}
+                            onChange={(e) => setZipCodeInputValue(e.target.value)}/>
                         </Fieldset>
 
                         <Label htmlFor="department">Department</Label>
