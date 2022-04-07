04a34c6752b114dbb2911f3e4bac2fb1d271f7cd
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-04-07 18:48:40 +0200

    Ajout de données pour le tableau

--- src/source/fakeData.js
+++ src/source/fakeData.js
@@ -53,6 +53,72 @@ const FakeData = [
         city: "Roundie",
         state: "Texas",
         zipCode: "45200"
+    },
+    {
+        firstName: "Pierre",
+        lastName: "Fromantin",
+        startDate: "18/03/2021",
+        department: "Sales",
+        birthDate: "23/07/1971",
+        street: "Wisteria Lane",
+        city: "Fairview",
+        state: "Alabama",
+        zipCode: "87000"
+    },
+    {
+        firstName: "Julie",
+        lastName: "Versaire",
+        startDate: "01/01/2022",
+        department: "Human Resources",
+        birthDate: "02/11/1968",
+        street: "5th Avenue",
+        city: "New York",
+        state: "New York",
+        zipCode: "75120"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith",
+        startDate: "30/04/2019",
+        department: "Engineering",
+        birthDate: "29/08/1958",
+        street: "Paradise Alley",
+        city: "New Heaven",
+        state: "California",
+        zipCode: "50777"
+    },
+    {
+        firstName: "Randy",
+        lastName: "Jones",
+        startDate: "06/10/2020",
+        department: "Sales",
+        birthDate: "14/02/1980",
+        street: "Freedom Street",
+        city: "Movetown",
+        state: "Kentucky",
+        zipCode: "30600"
+    },
+    {
+        firstName: "Gaby",
+        lastName: "Mayer",
+        startDate: "15/12/2015",
+        department: "Engineering",
+        birthDate: "11/09/1973",
+        street: "Donut Street",
+        city: "Roundie",
+        state: "Texas",
+        zipCode: "45200"
+    },
+    {
+        firstName: "Mike",
+        lastName: "Delfino",
+        startDate: "15/12/2015",
+        department: "Engineering",
+        birthDate: "11/09/1973",
+        street: "Donut Street",
+        city: "Roundie",
+        state: "Texas",
+        zipCode: "45200"
     }
 ]
 
