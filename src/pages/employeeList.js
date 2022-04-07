b3d54eb6c9be2004942b05223513de6dfd40868f
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-04-07 18:50:36 +0200

    Finition du tableau et de sa page

--- src/pages/employeeList.js
+++ src/pages/employeeList.js
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import styled from 'styled-components'
 // Imports to use the table in the page.
-import { useMemo } from 'react'
-import { useTable, useSortBy, usePagination } from 'react-table'
+import { useMemo, useState } from 'react'
+import { useTable, useSortBy, usePagination, useGlobalFilter, useAsyncDebounce } from 'react-table'
 import FakeData from '../source/fakeData'
 
 // Use of styled components.
@@ -58,6 +58,27 @@ const Pagispan = styled.span`
     }
 `
 
+// Define a component for global filtering.
+function GlobalFilter({globalFilter, setGlobalFilter}) {
+    const [value, setValue] = useState(globalFilter)
+    const onChange = useAsyncDebounce(value => {
+        setGlobalFilter(value || undefined)
+    }, 200)
+
+    return (
+        <div>Search:&nbsp;
+            <Input
+                type="text"
+                value={value || ""}
+                onChange={e => {
+                    setValue(e.target.value)
+                    onChange(e.target.value)
+                }}
+            />
+        </div>
+    )
+}
+
 const EmployeeList = () => {
     // Fake data to use the table in the page.
     const data = useMemo(() => FakeData, [])
@@ -107,8 +128,8 @@ const EmployeeList = () => {
     // We use the hook "useTable" with the data and the columns defined above to create a table instance.
     // In this first hook, we add the hooks we need to have more functionalities.
     const tableInstance = useTable(
-        {columns, data, initialState: {pageIndex: 0, rowIndex: 0}},
-        useSortBy, usePagination
+        {columns, data, initialState: {pageIndex: 0}},
+        useGlobalFilter, useSortBy, usePagination
     )
 
     // We define the props we will use in the table.
@@ -116,7 +137,7 @@ const EmployeeList = () => {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        //rows, // Without pagination.
+        rows,
         page, // "page" has only the rows for the active page.
         prepareRow,
         // The following are used for pagination.
@@ -126,8 +147,11 @@ const EmployeeList = () => {
         nextPage,
         pageOptions,
         setPageSize,
-        state: {pageSize, pageIndex, rowIndex}
-        // Above can be replaced by "state: {pageIndex, pageSize}" to display the number of the page.
+        state: {pageSize, pageIndex},
+        // The following are used for global filtering.
+        preGlobalFilteredRows,
+        setGlobalFilter,
+        state
     } = tableInstance
 
     // This function checks if the table is empty.
@@ -154,19 +178,22 @@ const EmployeeList = () => {
                         Show&nbsp;
                         <select name="entries" id="entries" value={pageSize}
                         onChange={e => {setPageSize(Number(e.target.value))}}>
-                            {[2, 3, 4, 10, 25, 50, 100].map(pageSize => (
+                            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 100].map(pageSize => (
                                 <option key={pageSize} value={pageSize}>{pageSize}</option>
                             ))}
                         </select>
                         &nbsp;entries
                     </div>
-                    <div>Search: <Input type="text"/></div>
-                    {/* Pour Search, utiliser GlobalFilter. */}
+                    <GlobalFilter
+                        preGlobalFilteredRows={preGlobalFilteredRows}
+                        globalFilter={state.globalFilter}
+                        setGlobalFilter={setGlobalFilter}
+                    />
                 </AboveTable>
 
                 {/* Below is the structure and the style of the table. */}
                 {/* Apply the table props. */}
-                <Table {...getTableProps()}>
+                <Table id="employee-table" {...getTableProps()}>
                     <thead>
                         {// Loop over the header rows.
                         headerGroups.map(headerGroup => (
@@ -215,14 +242,55 @@ const EmployeeList = () => {
                 </Table>
 
                 <Pagination>
-                    {/*First possible display.
-                    <div>Showing page {pageIndex + 1} of {pageOptions.length}</div>*/}
-                    {pageOptions.length === 1 ? (
-                        <div>Showing {page.length} of {page.length} entries</div>
+                    {/*First possible display. Example : Showing page 2 of 5.
+                    <div>Showing page {pageIndex + 1} of {pageOptions.length}.</div>*/}
+
+                    {/* Is there no data ? */}
+                    {rows.length === 0 ? (
+                        // If yes.
+                        <div>Showing 0 to 0 of 0 entry.</div>
                     ) : (
-                        //<div>Showing {page.length} of ~{pageOptions.length * pageSize} entries</div>
-                        <div>Showing {(pageIndex + 1) + pageIndex} to {(pageIndex + 1) * pageSize} of
-                        ~{pageOptions.length * pageSize} entries</div>
+                        // If not, is there only one page for all the entries ?
+                        pageOptions.length === 1 ? (
+                            // If yes, is there only one row in the page ?
+                            rows.length === 1 ? (
+                                // If yes.
+                                <div>Showing 1 to 1 of 1 entry.</div>
+                            ) : (
+                                // If not, "page.length" is the number of rows in the page.
+                                <div>Showing 1 to {page.length} of {page.length} entries.</div>
+                            )
+                        ) : (
+                            // If not, is the page length different from the "pageSize"
+                            // (defined by the select in <AboveTable/>) ?
+                            // NB : This case is possible only in the last page !
+                            page.length !== pageSize ? (
+                                // If yes, is there only one row in that page ?
+                                page.length === 1 ? (
+                                    // If yes :
+                                    // Example with a table containing 11 entries in 2 pages with a pageSize of 10.
+                                    // "Showing {(1 * 10) + 1} to {(1 * 10) + 1} of {rows.length} entries." means
+                                    // "Showing 11 to 11 of 11 entries." ("rows.length" is the total number of rows.)
+                                    <div>Showing {(pageIndex * pageSize) + 1} to {(pageIndex * pageSize) + 1} of&nbsp;
+                                    {rows.length} entries.</div>
+                                ) : (
+                                    // If not :
+                                    // Example with a table containing 11 entries in 4 pages with a pageSize of 3.
+                                    // "Showing {(3 * 3) + 1} to {((3 * 3) + 1) + (2 - 1)} of {rows.length} entries." means
+                                    // "Showing 10 to 11 of 11 entries."
+                                    <div>Showing {(pageIndex * pageSize) + 1} to {((pageIndex * pageSize) + 1) + (page.length - 1)}
+                                    &nbsp;of {rows.length} entries.</div>
+                                )
+                                
+                            ) : (
+                                // If not (meaning the page is full) :
+                                // Page 3 of the above example will be
+                                // "Showing {(2 * 3) + 1} to {(2 + 1) * 3} of {rows.length} entries." which means
+                                // "Showing 7 to 9 of 11 entries."
+                                <div>Showing {(pageIndex * pageSize) + 1} to {(pageIndex + 1) * pageSize} of&nbsp;
+                                {rows.length} entries.</div>
+                            )
+                        )
                     )}
                     <div>
                         <Pagispan onClick={() => previousPage()}
@@ -233,8 +301,6 @@ const EmployeeList = () => {
                 </Pagination>
 
                 <Link to="/" style={{marginTop: "40px"}}>Home</Link>
-                
-                <p>Ci-dessus, tableau à tester et modifier pour être le futur tableau.</p>
             </Container>
         </HelmetProvider>)
 }
