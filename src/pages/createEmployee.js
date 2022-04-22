0fbd298b0ef1efc8c105e65ad6e0d208fb9c0f8c
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-04-22 18:45:30 +0200

    Test d'install de librairie et du dropdown

--- src/pages/createEmployee.js
+++ src/pages/createEmployee.js
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import styled from 'styled-components'
+import '../index.css'
+// Imports to use the datetime library in the page.
 import Datetime from 'react-datetime'
 import 'react-datetime/css/react-datetime.css'
-import '../index.css'
 // Import to use the select library in the page.
 import Select from 'react-select'
 // Import to use my own dropdown in the page.
 import Dropdown from '../components/Dropdown'
+// Import to use my own modal library in the page.
+//import Modale from 'react-modale-jf'
 
 // Use of styled components.
 const Title = styled.div`
@@ -58,6 +61,8 @@ const CreateEmployee = () => {
     const [zipCodeInputValue, setZipCodeInputValue] = useState("")
     const [deptSelectedOption, setDeptSelectedOption] = useState(null)
     // The different "useState" above allow to keep the data of the different fields in the form.
+    //const [open, setOpen] = useState(false)
+    // This last one is used by the modal at the end of the page.
 
     // This function (TO FINISH !!) displays an error message when a value in a field is invalid
     // or a success message when everything is fine.
@@ -96,6 +101,7 @@ const CreateEmployee = () => {
         }
         // Ici, lancer la modale avec le message plutôt qu'une boite d'alerte...
         else {alert("Employee Created!")}
+        //else {displayModale()}
     }
 
     // Customization of the input in the <Datetime/> component.
@@ -203,6 +209,85 @@ const CreateEmployee = () => {
           return { ...provided, opacity, transition };
         }
     }
+
+    const stateChoices = [
+        {value: "alabama", label: "Alabama"},
+        {value: "alaska", label: "Alaska"},
+        {value: "american samoa", label: "American Samoa"},
+        {value: "arizona", label: "Arizona"},
+        {value: "arkansas", label: "Arkansas"},
+        {value: "california", label: "California"},
+        {value: "colorado", label: "Colorado"},
+        {value: "connecticut", label: "Connecticut"},
+        {value: "delaware", label: "Delaware"},
+        {value: "district of columbia", label: "District Of Columbia"},
+        {value: "federated states of micronesia", label: "Federated States Of Micronesia"},
+        {value: "florida", label: "Florida"},
+        {value: "georgia", label: "Georgia"},
+        {value: "guam", label: "Guam"},
+        {value: "hawaii", label: "Hawaii"},
+        {value: "idaho", label: "Idaho"},
+        {value: "illinois", label: "Illinois"},
+        {value: "indiana", label: "Indiana"},
+        {value: "iowa", label: "Iowa"},
+        {value: "kansas", label: "Kansas"},
+        {value: "kentucky", label: "Kentucky"},
+        {value: "louisiana", label: "Louisiana"},
+        {value: "maine", label: "Maine"},
+        {value: "marshall islands", label: "Marshall Islands"},
+        {value: "maryland", label: "Maryland"},
+        {value: "massachusetts", label: "Massachusetts"},
+        {value: "michigan", label: "Michigan"},
+        {value: "minnesota", label: "Minnesota"},
+        {value: "mississippi", label: "Mississippi"},
+        {value: "missouri", label: "Missouri"},
+        {value: "montana", label: "Montana"},
+        {value: "nebraska", label: "Nebraska"},
+        {value: "nevada", label: "Nevada"},
+        {value: "new hampshire", label: "New Hampshire"},
+        {value: "new jersey", label: "New Jersey"},
+        {value: "new mexico", label: "New Mexico"},
+        {value: "new york", label: "New York"},
+        {value: "north carolina", label: "North Carolina"},
+        {value: "north dakota", label: "North Dakota"},
+        {value: "northern mariana islands", label: "Northern Mariana Islands"},
+        {value: "ohio", label: "Ohio"},
+        {value: "oklahoma", label: "Oklahoma"},
+        {value: "oregon", label: "Oregon"},
+        {value: "palau", label: "Palau"},
+        {value: "pennsylvania", label: "Pennsylvania"},
+        {value: "puerto rico", label: "Puerto Rico"},
+        {value: "rhode island", label: "Rhode Island"},
+        {value: "south carolina", label: "South Carolina"},
+        {value: "south dakota", label: "South Dakota"},
+        {value: "tennessee", label: "Tennessee"},
+        {value: "texas", label: "Texas"},
+        {value: "utah", label: "Utah"},
+        {value: "vermont", label: "Vermont"},
+        {value: "virgin islands", label: "Virgin Islands"},
+        {value: "virginia", label: "Virginia"},
+        {value: "washington", label: "Washington"},
+        {value: "west virginia", label: "West Virginia"},
+        {value: "wisconsin", label: "Wisconsin"},
+        {value: "wyoming", label: "Wyoming"}
+    ]
+    const deptChoices = [
+        {value: "sales", label: "Sales"},
+        {value: "marketing", label: "Marketing"},
+        {value: "engineering", label: "Engineering"},
+        {value: "human resources", label: "Human Resources"},
+        {value: "legal", label: "Legal"}
+    ]
+
+    // The 2 functions below are used by the modal library I created.
+    /*
+    function displayModale() {
+        setOpen(true)
+    }
+    function closeModale() {
+        setOpen(false)
+    }
+    */
     
     return (
         <HelmetProvider>
@@ -268,10 +353,12 @@ const CreateEmployee = () => {
 
                     <Button onClick={saveEmployee}>Save</Button>
 
-                    <p>Ci-dessous, menu déroulant à venir...</p>
-                    <Dropdown/>
+                    <p>Ci-dessous, menu déroulant "states" à venir...</p>
+                    <Dropdown choices={stateChoices}/>
+                    <p>Ci-dessous, menu déroulant "depts" à venir...</p>
+                    <Dropdown choices={deptChoices}/>
                 </Container>
-                {/*<div id="confirmation" className="modal">Employee Created!</div>*/}
+                {/*<Modale message="Employee Created!" open={open} onClose={closeModale}/>*/}
             </main>
         </HelmetProvider>
     )
