40e7877b0a620162c170ebfbeb789f482d78640f
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-04-28 18:23:15 +0200

    Amélioration des données source

--- src/source/fakeData.js
+++ src/source/fakeData.js
@@ -3,122 +3,122 @@ const FakeData = [
         firstName: "Jérôme",
         lastName: "Fromantin",
         startDate: "18/03/2021",
-        department: "Sales",
+        department: "Human Resources",
         birthDate: "23/07/1971",
         street: "Wisteria Lane",
         city: "Fairview",
         state: "Alabama",
-        zipCode: "87000"
+        zipCode: "13140"
     },
     {
         firstName: "Annie",
         lastName: "Versaire",
         startDate: "01/01/2022",
-        department: "Human Resources",
+        department: "Marketing",
         birthDate: "02/11/1968",
         street: "5th Avenue",
         city: "New York",
         state: "New York",
-        zipCode: "75120"
+        zipCode: "12000"
     },
     {
         firstName: "Michael",
         lastName: "Smith",
-        startDate: "30/04/2019",
-        department: "Engineering",
+        startDate: "25/06/2009",
+        department: "Sales",
         birthDate: "29/08/1958",
         street: "Paradise Alley",
         city: "New Heaven",
         state: "California",
-        zipCode: "50777"
+        zipCode: "40390"
     },
     {
         firstName: "Brian",
         lastName: "Jones",
         startDate: "06/10/2020",
-        department: "Sales",
+        department: "Engineering",
         birthDate: "14/02/1980",
         street: "Freedom Street",
         city: "Movetown",
         state: "Kentucky",
-        zipCode: "30600"
+        zipCode: "11280"
     },
     {
         firstName: "Philip",
         lastName: "Mayer",
         startDate: "15/12/2015",
-        department: "Engineering",
+        department: "Legal",
         birthDate: "11/09/1973",
         street: "Donut Street",
         city: "Roundie",
         state: "Texas",
-        zipCode: "45200"
+        zipCode: "67800"
     },
     {
-        firstName: "Pierre",
-        lastName: "Fromantin",
-        startDate: "18/03/2021",
+        firstName: "Carmen",
+        lastName: "Smith",
+        startDate: "30/08/2018",
         department: "Sales",
-        birthDate: "23/07/1971",
-        street: "Wisteria Lane",
-        city: "Fairview",
-        state: "Alabama",
-        zipCode: "87000"
+        birthDate: "13/10/1974",
+        street: "Rock Street",
+        city: "Denver",
+        state: "Colorado",
+        zipCode: "26000"
     },
     {
         firstName: "Julie",
-        lastName: "Versaire",
-        startDate: "01/01/2022",
-        department: "Human Resources",
-        birthDate: "02/11/1968",
-        street: "5th Avenue",
-        city: "New York",
-        state: "New York",
-        zipCode: "75120"
+        lastName: "Mayer",
+        startDate: "21/12/2012",
+        department: "Marketing",
+        birthDate: "15/04/1975",
+        street: "New Land Alley",
+        city: "Columbus",
+        state: "Ohio",
+        zipCode: "10000"
     },
     {
-        firstName: "John",
-        lastName: "Smith",
-        startDate: "30/04/2019",
+        firstName: "Brian",
+        lastName: "Trenton",
+        startDate: "06/08/2010",
         department: "Engineering",
-        birthDate: "29/08/1958",
-        street: "Paradise Alley",
-        city: "New Heaven",
+        birthDate: "17/05/1977",
+        street: "Church Street",
+        city: "Sacramento",
         state: "California",
-        zipCode: "50777"
+        zipCode: "40000"
     },
     {
         firstName: "Randy",
         lastName: "Jones",
-        startDate: "06/10/2020",
-        department: "Sales",
-        birthDate: "14/02/1980",
-        street: "Freedom Street",
-        city: "Movetown",
-        state: "Kentucky",
-        zipCode: "30600"
+        startDate: "11/09/2001",
+        department: "Human Resources",
+        birthDate: "09/03/1981",
+        street: "Nope Street",
+        city: "Petra",
+        state: "New York",
+        zipCode: "12410"
     },
     {
-        firstName: "Gaby",
-        lastName: "Mayer",
-        startDate: "15/12/2015",
-        department: "Engineering",
-        birthDate: "11/09/1973",
-        street: "Donut Street",
-        city: "Roundie",
-        state: "Texas",
-        zipCode: "45200"
+        firstName: "Annie",
+        lastName: "Much",
+        startDate: "18/01/2015",
+        department: "Legal",
+        birthDate: "11/01/1973",
+        street: "Lake Street",
+        city: "Hogan",
+        state: "California",
+        zipCode: "40520"
     },
     {
-        firstName: "Mike",
+        firstName: "Philip",
         lastName: "Delfino",
-        startDate: "15/12/2015",
+        startDate: "30/01/2004",
         department: "Engineering",
-        birthDate: "11/09/1973",
-        street: "Donut Street",
-        city: "Roundie",
-        state: "Texas",
-        zipCode: "45200"
+        birthDate: "11/06/1972",
+        street: "Horland Lane",
+        city: "Augusta",
+        state: "Maine",
+        zipCode: "79000"
     }
 ]
 
