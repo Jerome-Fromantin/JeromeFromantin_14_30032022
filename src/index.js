4b4a56123e3874e808dc7cdc6c4c648067a2cd87
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-05-05 18:08:48 +0200

    Ajout d'un <<ContextProvider>>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ContextProvider from './store/contextProvider'
 
 const container = document.getElementById('root')
 const root = ReactDOMClient.createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
